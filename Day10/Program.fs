5b006faffc95f7a7a1a17c941813333d568f5f19
mark.plunkett <markje.p@gmail.com>
2021-12-18 14:26:46 +0000

    Day10 part 2.

--- Day10/Program.fs
+++ Day10/Program.fs
@@ -11,29 +11,61 @@ let pairs = dict [
     '<', '>'
 ]
 
-let points = dict [
-    ')', 3
-    ']', 57
-    '}', 1197
-    '>', 25137
-]
+module Part1 =
 
-let scoreLine (line : string) =
-    line
-    |> Seq.scan (fun (closes, _) c ->
-        match pairs.TryGetValue c with
-        | true, close -> close :: closes, None
-        | false, _ -> 
-            // its a closing char
-            if List.head closes = c then List.tail closes, None
-            else closes, Some c
-    ) ([], None)
-    |> Seq.tryPick snd
-    |> Option.map (fun c -> points.[c])
+    let closeScores = dict [
+        ')', 3
+        ']', 57
+        '}', 1197
+        '>', 25137
+    ]
+
+    let scoreLine (line : string) =
+        line
+        |> Seq.scan (fun (closes, _) c ->
+            match pairs.TryGetValue c with
+            | true, close -> close :: closes, None
+            | false, _ -> 
+                // its a closing char
+                if List.head closes = c then List.tail closes, None
+                else closes, Some c
+        ) ([], None)
+        |> Seq.tryPick snd
+        |> Option.map (fun c -> closeScores.[c])
+
+    let solve input =
+        input
+        |> Seq.map scoreLine
+        |> Seq.choose id
+        |> Seq.sum
+
+module Part2 =
+
+    let closeScores = dict [
+        ')', 1L
+        ']', 2L
+        '}', 3L
+        '>', 4L
+    ]
+
+    let scoreLine (line : string) =
+        line
+        |> Seq.fold (fun closeChars openChar ->
+            match pairs.TryGetValue openChar with
+            | true, close -> close :: closeChars
+            | false, _ -> List.tail closeChars
+        ) []
+        |> List.fold (fun total c -> (5L * total) + closeScores.[c]) 0L
+
+    let solve input =
+        input
+        |> Seq.filter (Part1.scoreLine >> Option.isNone)
+        |> Seq.map scoreLine
+        |> Seq.sort
+        |> Seq.toArray
+        |> fun a -> a.[a.Length / 2]
 
 Seq.initInfinite (fun _ -> Console.ReadLine())
 |> Seq.takeWhile (String.IsNullOrEmpty >> not)
-|> Seq.map scoreLine
-|> Seq.choose id
-|> Seq.sum
-|> Console.WriteLine
+|> Part2.solve
+|> printfn "%A"
